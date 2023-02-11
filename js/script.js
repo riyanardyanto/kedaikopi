c909e1988c82f088f0367542e7f11ca7a5905a85
riyanardyanto <riyanardyanto@gmail.com>
2023-02-11 09:58:52 +0700

    a

--- js/script.js
+++ js/script.js
@@ -16,12 +16,14 @@ document.addEventListener("click", function (e) {
 // Googlesheet input
 const scriptURL = "https://script.google.com/macros/s/AKfycbw813lyYWh0FUdmmee2eU4DehPDiyLDjXVrLmHYef0Xzsio3wq8Ptw42mbTNGm8FVw/exec";
 const form = document.forms["google-sheet"];
+let inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => alert("You have successfully submitted."))
     .catch((error) => console.error("Error!", error.message));
+  inputs.forEach((input) => (input.value = ""));
 });
 
 // Clear input Fields
